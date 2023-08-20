23563797d7aa8ee01e048b3af09439783cb110e1
Gökbörü Efe Tığrel <efetigrel@gmail.com>
2023-08-20 23:38:44 +0300

    liste-uygulamasi

---
+++ liste-uygulamasi/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+var sehirler = new List<string>()
+{
+    "Ankara",
+    "Istanbul",
+    "Van",
+    "Samsun",
+    "Ordu",
+
+};
+
+/*
+    foreach (string s in sehirler)
+    {                               -----> Bunun yerine alttakini kullan               
+    Console.WriteLine(s); 
+    }
+*/
+
+// Lamda expression
+sehirler.ForEach(s => Console.WriteLine(s));
+
+
+Console.WriteLine(new string('-',50)); 
+
+
+var iller = sehirler;
+iller.ForEach(i => Console.WriteLine(i));
+
+
+Console.WriteLine(new string('-', 50));
+
+
+sehirler.Add("Sinop");
+sehirler.ForEach(s => Console.WriteLine(s));
+
+
+Console.WriteLine(new string('-', 50));
+
+
+iller.ForEach(i => Console.WriteLine(i));
+
+
+Console.WriteLine(new string('-', 50));
+
+
+iller.Remove("Ankara");
+iller.ForEach(i => Console.WriteLine(i));
+
+
+Console.WriteLine(new string('-', 50));
+
+
+sehirler.ForEach(s => Console.WriteLine(s));
+
+
+
+Console.ReadKey();
